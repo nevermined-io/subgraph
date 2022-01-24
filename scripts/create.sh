e8db61c8529c72cc5dd30ce32fb3e5e113c461e0
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-01-24 17:18:18 +0100

    Added dockerfile to build in dockerhub
    - Added compose-file for local dev and CI
    - Added tests for both spree and polygon

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+GRAPH_NODE_URL=${GRAPH_NODE_URL:-http://localhost:9020/}
+
 for d in ./subgraphs/*
 do
-    (cd "$d" && graph create --node http://localhost:9020/ neverminedio/$(basename "$d"))
+    (cd "$d" && graph create --node $GRAPH_NODE_URL neverminedio/$(basename "$d"))
 done
