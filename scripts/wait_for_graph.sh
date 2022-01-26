88ce17345aec36a1138e8b4c7a9b5c4f028fee8b
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-01-26 11:43:05 +0100

    Fix dockerfile

--- scripts/wait_for_graph.sh
+++ scripts/wait_for_graph.sh
@@ -2,14 +2,14 @@
 
 RETRY_COUNT=0
 HTTP_CODE=0
-API_URL=http://localhost:9020
+GRAPH_NODE_URL=${GRAPH_NODE_URL:-http://localhost:9020/}
 
 until [ $HTTP_CODE -eq 200 ] || [ $RETRY_COUNT -eq 240 ]; do
-  HTTP_CODE=$(curl -s -o /dev/null -w ''%{http_code}'' -X OPTIONS $API_URL)
+  HTTP_CODE=$(curl -s -o /dev/null -w ''%{http_code}'' -X OPTIONS $GRAPH_NODE_URL)
   if [ $HTTP_CODE -eq 200 ]; then
     break
   fi
-  printf "Waiting for the graph node api to be running at $API_URL\n"
+  printf "Waiting for the graph node api to be running at $GRAPH_NODE_URL\n"
   sleep 10
   let RETRY_COUNT=RETRY_COUNT+1
 done
