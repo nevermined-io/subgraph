7d1367dfc383715823ab15bd4f7c79d0e745777c
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-02-04 13:47:28 +0100

    Added support for matic
    - Now update-startblock also works on matic

--- scripts/update-startblock.sh
+++ scripts/update-startblock.sh
@@ -2,12 +2,18 @@
 
 # This script requires the jq and yq clis
 
-POLYGONSCAN_URL=${POLYGONSCAN_URL:- https://api-testnet.polygonscan.com/api}
+POLYGONSCAN_URL_MUMBAI=${POLYGONSCAN_URL_MUMBAI:-https://api-testnet.polygonscan.com/api}
+POLYGONSCAN_URL_MATIC=${POLYGONSCAN_URL_MATIC:-https://api.polygonscan.com/api}
+POLYGONSCAN_APIKEY=${POLYGONSCAN_APIKEY:-ISP8J7YJGP88XSHU55QJF3YM71WVFRWSDJ}
 GRAPH_NODE_URL=${GRAPH_NODE_URL:-http://localhost:9020/}
 NETWORK=$1
 
-if [ "$NETWORK" != "mumbai" ]; then
-    echo "Currently only supported in mumbai. Skipping..."
+if [ "$NETWORK" == "mumbai" ]; then
+    POLYGONSCAN_URL=$POLYGONSCAN_URL_MUMBAI
+elif [ "$NETWORK" == "matic" ]; then
+    POLYGONSCAN_URL=$POLYGONSCAN_URL_MATIC
+else
+    echo "Currently not supported in $NETWORK. Skipping..."
     exit 0
 fi
 
@@ -17,7 +23,7 @@ do
     # Get the implementation address and block number where contract was created
     BLOCK_NUMBER=10
     BASENAME=$(basename "$d")
-    ABI=node_modules/@nevermined-io/contracts/artifacts/$BASENAME.mumbai.json
+    ABI=node_modules/@nevermined-io/contracts/artifacts/$BASENAME.$NETWORK.json
     SUBGRAPH=$d/subgraph.yaml
     IMPLEMENTATION_ADDRESS=$(cat $ABI | jq -r '.implementation')
     BLOCK_NUMBER=$(
@@ -30,7 +36,7 @@ do
         -d page=1 \
         -d offset=10 \
         -d sort=asc \
-        -d apiKey=YourApiKeyToken | jq -r '.result[0].blockNumber' || echo 10
+        -d apiKey=$POLYGONSCAN_APIKEY | jq -r '.result[0].blockNumber' || echo 10
     )
     echo $BASENAME
     echo $ABI
@@ -40,6 +46,9 @@ do
 
     # update the startBlock
      yq -i ".dataSources[0].source.startBlock = $BLOCK_NUMBER" $SUBGRAPH
+
+    # try not to trigger any rate-limits
+    sleep 0.5
 
 
 done
