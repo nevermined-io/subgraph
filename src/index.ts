63530d3db63cfaea756b9fcdee5f510d8301be13
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-02-05 18:15:34 +0100

    Export the new clients in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,38 @@
-import * as AaveBorrowCondition from './AaveBorrowCondition'
-import * as AaveCollateralDepositCondition from './AaveCollateralDepositCondition'
-import * as AaveCollateralWithdrawCondition from './AaveCollateralWithdrawCondition'
-import * as AaveCreditTemplate from './AaveCreditTemplate'
-import * as AaveRepayCondition from './AaveRepayCondition'
-import * as AccessCondition from './AccessCondition'
-import * as AccessTemplate from './AccessTemplate'
-import * as AgreementStoreManager from './AgreementStoreManager'
-import * as ComputeExecutionCondition from './ComputeExecutionCondition'
-import * as DIDRegistry from './DIDRegistry'
-import * as DIDSalesTemplate from './DIDSalesTemplate'
-import * as Dispenser from './Dispenser'
-import * as DistributeNFTCollateralCondition from './DistributeNFTCollateralCondition'
-import * as EscrowComputeExecutionTemplate from './EscrowComputeExecutionTemplate'
-import * as HashLockCondition from './HashLockCondition'
-import * as NeverminedToken from './NeverminedToken'
-import * as NFT721AccessTemplate from './NFT721AccessTemplate'
-import * as NFT721HolderCondition from './NFT721HolderCondition'
-import * as NFT721LockCondition from './NFT721LockCondition'
-import * as NFT721SalesTemplate from './NFT721SalesTemplate'
-import * as NFTAccessCondition from './NFTAccessCondition'
-import * as NFTAccessTemplate from './NFTAccessTemplate'
-import * as NFTHolderCondition from './NFTHolderCondition'
-import * as NFTLockCondition from './NFTLockCondition'
-import * as NFTSalesTemplate from './NFTSalesTemplate'
-import * as SignCondition from './SignCondition'
-import * as TemplateStoreManager from './TemplateStoreManager'
-import * as ThresholdCondition from './ThresholdCondition'
-import * as TransferNFT721Condition from './TransferNFT721Condition'
-import * as TransferNFTCondition from './TransferNFTCondition'
-import * as WhitelistingCondition from './WhitelistingCondition'
-import * as TransferDIDOwnershipCondition from './TransferDIDOwnershipCondition'
+import * as AaveBorrowCondition from "./AaveBorrowCondition";
+import * as AaveCollateralDepositCondition from "./AaveCollateralDepositCondition";
+import * as AaveCollateralWithdrawCondition from "./AaveCollateralWithdrawCondition";
+import * as AaveCreditTemplate from "./AaveCreditTemplate";
+import * as AaveRepayCondition from "./AaveRepayCondition";
+import * as AccessCondition from "./AccessCondition";
+import * as AccessProofCondition from "./AccessProofCondition";
+import * as AccessProofTemplate from "./AccessProofTemplate";
+import * as AccessTemplate from "./AccessTemplate";
+import * as AgreementStoreManager from "./AgreementStoreManager";
+import * as ComputeExecutionCondition from "./ComputeExecutionCondition";
+import * as DIDRegistry from "./DIDRegistry";
+import * as DIDSalesTemplate from "./DIDSalesTemplate";
+import * as Dispenser from "./Dispenser";
+import * as DistributeNFTCollateralCondition from "./DistributeNFTCollateralCondition";
+import * as EscrowComputeExecutionTemplate from "./EscrowComputeExecutionTemplate";
+import * as HashLockCondition from "./HashLockCondition";
+import * as NeverminedToken from "./NeverminedToken";
+import * as NFT721AccessTemplate from "./NFT721AccessTemplate";
+import * as NFT721HolderCondition from "./NFT721HolderCondition";
+import * as NFT721LockCondition from "./NFT721LockCondition";
+import * as NFT721SalesTemplate from "./NFT721SalesTemplate";
+import * as NFT721Upgradeable from "./NFT721Upgradeable";
+import * as NFTAccessCondition from "./NFTAccessCondition";
+import * as NFTAccessTemplate from "./NFTAccessTemplate";
+import * as NFTHolderCondition from "./NFTHolderCondition";
+import * as NFTLockCondition from "./NFTLockCondition";
+import * as NFTSalesTemplate from "./NFTSalesTemplate";
+import * as SignCondition from "./SignCondition";
+import * as TemplateStoreManager from "./TemplateStoreManager";
+import * as ThresholdCondition from "./ThresholdCondition";
+import * as TransferNFT721Condition from "./TransferNFT721Condition";
+import * as TransferNFTCondition from "./TransferNFTCondition";
+import * as WhitelistingCondition from "./WhitelistingCondition";
+import * as TransferDIDOwnershipCondition from "./TransferDIDOwnershipCondition";
 
 
 export {
@@ -39,6 +42,8 @@ export {
     AaveCreditTemplate,
     AaveRepayCondition,
     AccessCondition,
+    AccessProofCondition,
+    AccessProofTemplate,
     AccessTemplate,
     AgreementStoreManager,
     ComputeExecutionCondition,
@@ -53,6 +58,7 @@ export {
     NFT721HolderCondition,
     NFT721LockCondition,
     NFT721SalesTemplate,
+    NFT721Upgradeable,
     NFTAccessCondition,
     NFTAccessTemplate,
     NFTHolderCondition,
@@ -65,5 +71,5 @@ export {
     TransferNFTCondition,
     WhitelistingCondition,
     TransferDIDOwnershipCondition,
-}
+};
 
