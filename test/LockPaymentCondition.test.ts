5911cf29e656c31453381d8ca3f7bb7c4c4176ab
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-02-21 19:23:14 +0100

    Fix an issue with wrong conversion of addresses in LockPaymentCondition
    - bumped version to v0.2.8
    - fixed and issue with the conversion of the amounts in the
      lockPaymentCondition subgraph client

---
+++ test/LockPaymentCondition.test.ts
@@ -0,0 +1,125 @@
+import { assert } from 'chai'
+import { ApolloClient, InMemoryCache, gql, NormalizedCacheObject } from '@apollo/client/core'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import ws from 'ws'
+
+import { Account, DDO, MetaData, Nevermined} from '@nevermined-io/nevermined-sdk-js'
+import { didZeroX, zeroX } from '@nevermined-io/nevermined-sdk-js/dist/node/utils'
+
+import { config } from './config'
+import { getFulfilleds } from '../src/LockPaymentCondition'
+import { getMetadata } from './utils'
+import AssetRewards from '@nevermined-io/nevermined-sdk-js/dist/node/models/AssetRewards'
+
+
+let nevermined: Nevermined
+let publisher: Account
+let consumer: Account
+let wsClient: ApolloClient<NormalizedCacheObject>
+let metadata: MetaData
+let ddo: DDO
+let agreementId: string
+
+describe('LockPaymentCondition', () => {
+    before(async () => {
+        nevermined = await Nevermined.getInstance(config)
+        ;[publisher, consumer] = await nevermined.accounts.list()
+
+        metadata = getMetadata()
+
+        const subscriptionClient = new SubscriptionClient(
+            'ws://localhost:9001/subgraphs/name/neverminedio/LockPaymentCondition',
+            {
+                reconnect: true,
+            },
+            ws,
+
+        )
+        const webSocketLink = new WebSocketLink(subscriptionClient)
+        wsClient = new ApolloClient({
+            link: webSocketLink,
+            cache: new InMemoryCache(),
+        })
+    })
+
+    it('should register an NFT', async () => {
+        ddo = await nevermined.nfts.create(
+            metadata,
+            publisher,
+            10,
+            0,
+            new AssetRewards(new Map([
+                [publisher.getId(), 10],
+                [consumer.getId(), 10],
+            ]))
+        )
+        assert.isDefined(ddo)
+        await nevermined.nfts.mint(ddo.id, 1, publisher)
+    })
+
+    it('should order an NFT', async () =>{
+        await consumer.requestTokens(100)
+        agreementId = await nevermined.nfts.order(ddo.id, 1, consumer)
+        assert.isDefined(agreementId)
+    })
+
+    it('should subscribe and wait for LockPaymentCondition', async () => {
+        const query = `
+                subscription($agreementId: Bytes) {
+                    fulfilleds(where: {_agreementId: $agreementId}) {
+                        _did,
+                        _agreementId,
+                    }
+                }
+            `
+            const observable = wsClient.subscribe({
+                query: gql(query),
+                variables: {
+                    agreementId: zeroX(agreementId),
+                },
+            })
+
+            const promise = new Promise((resolve, reject) => {
+                observable.subscribe(
+                    x => {
+                        if (x.data.fulfilleds !== null && x.data.fulfilleds.length > 0) {
+                            resolve(x)
+                        }
+                    },
+                    err => reject(err),
+                )
+            })
+            const result: any = await promise
+            const event = result.data.fulfilleds[0]
+            assert.equal(event._did, didZeroX(ddo.id))
+            assert.equal(event._agreementId, agreementId)
+    })
+
+    it('should query the LockPaymentCondition Fulfilled event', async () => {
+        const response = await getFulfilleds(
+            'http://localhost:9000/subgraphs/name/neverminedio/LockPaymentCondition',
+            {
+                where: {
+                    _agreementId: zeroX(agreementId),
+                },
+
+            },
+            {
+                _agreementId: true,
+                _did: true,
+                _conditionId: true,
+                _rewardAddress: true,
+                _tokenAddress: true,
+                _receivers: true,
+                _amounts: true,
+            },
+        )
+        const event = response.pop()
+        assert.isDefined(event)
+        assert.equal(event._agreementId, agreementId)
+        assert.equal(event._did, didZeroX(ddo.id))
+        assert.equal(event._receivers[0], [publisher.getId().toLowerCase()][0])
+        assert.deepEqual(event._amounts.map(a => a.toNumber()), [10, 10])
+    })
+})
---
+++ test/utils.ts
@@ -0,0 +1,68 @@
+import { MetaData } from "@nevermined-io/nevermined-sdk-js"
+
+const metadata: Partial<MetaData> = {
+    main: {
+        name: undefined,
+        type: 'dataset',
+        dateCreated: '2012-10-10T17:00:00Z',
+        datePublished: '2012-10-10T17:00:00Z',
+        author: 'Met Office',
+        license: 'CC-BY',
+        price: '21' + '0'.repeat(18),
+        files: [
+            {
+                index: 0,
+                contentType: 'application/json',
+                url:
+                    'https://github.com/nevermined-io/docs/blob/master/docs/architecture/specs/metadata/examples/ddo-example.json'
+            },
+            {
+                index: 1,
+                contentType: 'text/plain',
+                url: 'https://github.com/nevermined-io/docs/blob/master/README.md'
+            }
+        ]
+    },
+    additionalInformation: {
+        description: 'Weather information of UK including temperature and humidity',
+        copyrightHolder: 'Met Office',
+        workExample: '423432fsd,51.509865,-0.118092,2011-01-01T10:55:11+00:00,7.2,68',
+        links: [
+            {
+                name: 'Sample of Asset Data',
+                type: 'sample',
+                url: 'https://foo.com/sample.csv'
+            },
+            {
+                name: 'Data Format Definition',
+                type: 'format',
+                url: 'https://foo.com/sample.csv'
+            }
+        ],
+        inLanguage: 'en',
+        categories: ['Economy', 'Data Science'],
+        tags: ['weather', 'uk', '2011', 'temperature', 'humidity']
+    }
+}
+
+export const generateMetadata = (
+    name: string,
+    price?: number,
+    nonce: string | number = Math.random()
+): Partial<MetaData> => ({
+    ...metadata,
+    main: {
+        ...metadata.main,
+        name: name,
+        price: (price || 21) + '0'.repeat(18),
+        ...({ nonce } as any)
+    },
+    additionalInformation: {
+        ...metadata.additionalInformation
+    }
+})
+
+export const getMetadata = (
+    price?: number,
+    nonce: string | number = Math.random()
+): MetaData => generateMetadata('TestAsset', price, nonce) as MetaData
