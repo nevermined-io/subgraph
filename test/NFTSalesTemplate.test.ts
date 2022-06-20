472bed4ad74f37818cc56fb4deda985b09f38c54
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-06-20 15:20:05 +0200

    Fix wrong type conversion with timelocks and timeouts
    - remove rc tag
    - test against --latest
    - added workflow for github releases

---
+++ test/NFTSalesTemplate.test.ts
@@ -0,0 +1,134 @@
+import { assert } from 'chai'
+import { ApolloClient, InMemoryCache, gql, NormalizedCacheObject } from '@apollo/client/core'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import ws from 'ws'
+
+import { Account, DDO, MetaData, Nevermined } from '@nevermined-io/nevermined-sdk-js'
+import { didZeroX, zeroX } from '@nevermined-io/nevermined-sdk-js/dist/node/utils'
+
+import { config } from './config'
+import { getFulfilleds } from '../src/LockPaymentCondition'
+import { getAgreementCreateds } from '../src/NFTSalesTemplate'
+import { getMetadata } from './utils'
+import AssetRewards from '@nevermined-io/nevermined-sdk-js/dist/node/models/AssetRewards'
+import BigNumber from 'bignumber.js'
+import { decodeJwt } from 'jose'
+
+
+let nevermined: Nevermined
+let publisher: Account
+let consumer: Account
+let wsClient: ApolloClient<NormalizedCacheObject>
+let metadata: MetaData
+let ddo: DDO
+let agreementId: string
+
+describe('NFTSalesTemplate', () => {
+    before(async () => {
+        nevermined = await Nevermined.getInstance(config)
+            ;[publisher, consumer] = await nevermined.accounts.list()
+
+        console.log(await publisher.getBalance())
+        metadata = getMetadata()
+
+        const clientAssertion = await nevermined.utils.jwt.generateClientAssertion(publisher)
+        await nevermined.marketplace.login(clientAssertion)
+        const payload = decodeJwt(config.marketplaceAuthToken)
+        metadata.userId = payload.sub
+
+        const subscriptionClient = new SubscriptionClient(
+            'ws://localhost:9001/subgraphs/name/neverminedio/NFTSalesTemplate',
+            {
+                reconnect: true,
+            },
+            ws,
+
+        )
+        const webSocketLink = new WebSocketLink(subscriptionClient)
+        wsClient = new ApolloClient({
+            link: webSocketLink,
+            cache: new InMemoryCache(),
+        })
+    })
+
+    it('should register an NFT', async () => {
+        ddo = await nevermined.nfts.create(
+            metadata,
+            publisher,
+            10,
+            0,
+            new AssetRewards(new Map([
+                [publisher.getId(), new BigNumber(10)],
+                [consumer.getId(), new BigNumber(10)],
+            ]))
+        )
+        assert.isDefined(ddo)
+    })
+
+    it('should order an NFT', async () => {
+        await consumer.requestTokens(100)
+        agreementId = await nevermined.nfts.order(ddo.id, 1, consumer)
+        assert.isDefined(agreementId)
+    })
+
+    it('should subscribe and wait for NFTSalesTemplate event', async () => {
+        const query = `
+                subscription($agreementId: Bytes) {
+                    agreementCreateds(where: {_agreementId: $agreementId}) {
+                        _did,
+                        _agreementId,
+                    }
+                }
+            `
+        const observable = wsClient.subscribe({
+            query: gql(query),
+            variables: {
+                agreementId: zeroX(agreementId),
+            },
+        })
+
+        const promise = new Promise((resolve, reject) => {
+            observable.subscribe(
+                x => {
+                    if (x.data.agreementCreateds !== null && x.data.agreementCreateds.length > 0) {
+                        resolve(x)
+                    }
+                },
+                err => reject(err),
+            )
+        })
+        const result: any = await promise
+        const event = result.data.agreementCreateds[0]
+        assert.equal(event._did, didZeroX(ddo.id))
+        assert.equal(event._agreementId, agreementId)
+    })
+
+    it('should query the NFTSalesTemplate agreement created event', async () => {
+        const response = await getAgreementCreateds(
+            'http://localhost:9000/subgraphs/name/neverminedio/NFTSalesTemplate',
+            {
+                where: {
+                    _agreementId: zeroX(agreementId),
+                },
+
+            },
+            {
+                _accessConsumer: true,
+                _accessProvider: true,
+                _agreementId: true,
+                _conditionIdSeeds: true,
+                _conditionIds: true,
+                _creator: true,
+                _did: true,
+                _idSeed: true,
+                _timeLocks: true,
+                _timeOuts: true
+            },
+        )
+        const event = response.pop()
+        assert.isDefined(event)
+        assert.equal(event!._agreementId, agreementId)
+        assert.equal(event!._did, didZeroX(ddo.id))
+    })
+})
